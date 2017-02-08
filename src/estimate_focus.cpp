ca14d4da2aafbc595a4f77ab13c062d82ca4e380
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2017-02-08 17:03:37 +0000

    Added computation of attention focus on playground zones

--- src/estimate_focus.cpp
+++ src/estimate_focus.cpp
@@ -6,8 +6,10 @@
 
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 #include <sensor_msgs/Range.h>
 #include <std_msgs/ColorRGBA.h>
+#include <geometry_msgs/Point.h>
 #include <tf/transform_listener.h>
 
 #include <dynamic_reconfigure/server.h>
@@ -27,6 +29,7 @@ static const float RANGE = 3.; //m
 // of the gaze axis permitted by the field of view FOV (ie, at the limit of the field of view)
 static const double ATTENTION_LEVEL_AT_FOV_LIMIT = 0.01;
 
+
 inline double gaussian(double x, double mean, double variance) {
     return exp(-(x-mean)*(x-mean)/(2*variance));
 }
@@ -39,6 +42,51 @@ constexpr std::chrono::milliseconds to_ms(const std::chrono::duration<Rep,Period
     return std::chrono::duration_cast<std::chrono::milliseconds>(d);
 }
 
+typedef std::vector<geometry_msgs::Point> Region;
+
+std::map<std::string, std::vector<Region>> zones;
+
+/**
+ * Based on http://paulbourke.net/geometry/polygonmesh/
+ */
+bool isIn(tf::Vector3 p, Region region) {
+
+    bool inside = false;
+    double xinters;
+
+    auto p1 = region[0];
+
+    for (size_t i = 1; i <= region.size(); i++) {
+        auto p2 = region[i % region.size()];
+
+        if (p.y() > min(p1.y,p2.y)) {
+            if (p.y() <= max(p1.y,p2.y)) {
+                if (p.x() <= max(p1.x,p2.x)) {
+                    if (p1.y != p2.y) {
+                        xinters = (p.y()-p1.y)*(p2.x-p1.x)/(p2.y-p1.y)+p1.x;
+                        if (p1.x == p2.x || p.x() <= xinters) {
+                            inside = !inside;
+                        }
+                    }
+                }
+            }
+        }
+        p1 = p2;
+    }
+    return inside;
+}
+
+void on_zones_received(const visualization_msgs::MarkerArrayConstPtr& markers) {
+
+    ROS_INFO("Received playground zones");
+    zones.clear();
+
+    for (const auto& m : markers->markers) {
+        zones[m.ns].push_back(m.points);
+    }
+
+}
+
 map<std::string, std::chrono::system_clock::time_point> last_seen_frames;
 
 void callback(playground_builder::estimate_focusConfig &config, uint32_t level) {
@@ -47,7 +95,11 @@ void callback(playground_builder::estimate_focusConfig &config, uint32_t level) 
     MIN_ATTENTIONAL_SPAN = chrono::milliseconds(config.attentional_span);
 }
 
-visualization_msgs::Marker makeMarker(int id, const string& frame, std_msgs::ColorRGBA color) {
+visualization_msgs::Marker makeMarker(int id,
+                                      const string& frame, 
+                                      std_msgs::ColorRGBA color, 
+                                      double dx=0, double dy=0, double dz=0,
+                                      const string& ns = "focus_of_attention") {
 
     visualization_msgs::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
@@ -56,7 +108,7 @@ visualization_msgs::Marker makeMarker(int id, const string& frame, std_msgs::Col
 
     // Set the namespace and id for this marker.  This serves to create a unique ID
     // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "focus_of_attention";
+    marker.ns = ns;
     marker.id = id;
 
     marker.type = visualization_msgs::Marker::SPHERE;
@@ -65,9 +117,9 @@ visualization_msgs::Marker makeMarker(int id, const string& frame, std_msgs::Col
     marker.action = visualization_msgs::Marker::ADD;
 
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
+    marker.pose.position.x = dx;
+    marker.pose.position.y = dy;
+    marker.pose.position.z = dz;
     marker.pose.orientation.x = 0.0;
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
@@ -84,14 +136,58 @@ visualization_msgs::Marker makeMarker(int id, const string& frame, std_msgs::Col
     return marker;
 }
 
-double fieldOfViewDistance(const tf::TransformListener& listener, 
-                           const string& target_frame, 
-                           const string& observer_frame) {
+
+
+
+tf::Vector3 gazePlanIntersection(const tf::TransformListener& listener, 
+                                 const std::string& reference_frame, 
+                                 const std::string& gaze_frame) {
+
+    tf::StampedTransform transform;
+
+    // transform the candidate target to the observer's frame of reference
+    try{
+        listener.lookupTransform(reference_frame, gaze_frame, ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+        ros::Duration(1.0).sleep();
+    }
+
+
+    // from now on, everything takes place in the reference frame (ie, the target plan)
+
+    auto gaze_origin = transform.getOrigin();
+
+    auto gaze_vector = transform * tf::Vector3(1,0,0) - gaze_origin;
+    gaze_vector.normalize();
+
+    tf::Vector3 plane_normal(0,0,1);
+    plane_normal.normalize(); // trivial when the vector is 0,0,1
+    double distance_plane_to_origin = 0.;
+
+    auto t = - (gaze_origin.dot(plane_normal) + distance_plane_to_origin) / (gaze_vector.dot(plane_normal));
+
+    auto gaze_projection = gaze_origin + gaze_vector * t;
+
+    return gaze_projection;
+
+}
+
+
+
+
+
+
+double intensityOfVisualAttention(const tf::TransformListener& listener, 
+                                  const string& target_frame, 
+                                  const string& observer_frame) {
 
     if (target_frame == observer_frame) return 0;
 
     tf::StampedTransform transform;
 
+    // transform the candidate target to the observer's frame of reference
     try{
         listener.lookupTransform(observer_frame, target_frame, ros::Time(0), transform);
     }
@@ -112,6 +208,9 @@ double fieldOfViewDistance(const tf::TransformListener& listener,
 
     //ROS_INFO_STREAM(target_frame << ": distance_to_main_axis: " << distance_to_main_axis << ", fov_radius_at_x: " << fov_radius_at_x);
     
+    // compute the variance of the gaussian distribution so that whenever 
+    // distance_to_main_axis == fov_radius_at_x, the resulting attention level is
+    // ATTENTION_LEVEL_AT_FOV_LIMIT
     double variance = - fov_radius_at_x / (2*log(ATTENTION_LEVEL_AT_FOV_LIMIT));
 
     return gaussian(distance_to_main_axis, 0, variance);
@@ -145,6 +244,9 @@ int main( int argc, char** argv )
 
     ros::NodeHandle n;
     ros::Rate r(30);
+
+    auto zones_sub = n.subscribe("zones", 1, on_zones_received);
+
     ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("estimate_focus", 1);
     ros::Publisher fov_pub = n.advertise<sensor_msgs::Range>("face_0_field_of_view", 1);
     ros::Publisher attentional_targets_pub = 
@@ -189,6 +291,7 @@ int main( int argc, char** argv )
             r.sleep();
         }
 
+
         frames.clear();
         listener.getFrameStrings(frames);
 
@@ -207,11 +310,56 @@ int main( int argc, char** argv )
                     targets.header.frame_id = frame;
                     targets.header.stamp = ros::Time::now();
 
+                    //////////////////////////////////////////
+                    // gaze projection on sandtray
+                    auto gaze_projection = gazePlanIntersection(listener, "sandtray", frame);
+
+                    std_msgs::ColorRGBA col;
+                    col.r = 1.; col.g = 0.; col.b = 0.; col.a = 1.;
+
+                    marker_pub.publish(makeMarker(1000 + face_idx, 
+                                                "sandtray", 
+                                                col, 
+                                                gaze_projection[0], gaze_projection[1],0,
+                                                "gaze_projection"));
+
+                    // Does the gaze falls on one existing playground zone?
+                    // If yes, publish it as a new active attention target
+                    ROS_INFO_STREAM("nb zones: " << zones.size());
+                    for (const auto& kv : zones) {
+                        bool isinzone = false;
+                        for (const auto& region : kv.second) {
+
+                            isinzone = isIn(gaze_projection, region);
+
+                            if (isinzone) break;
+                        }
+                        if (isinzone) {
+
+                            col.r = 0.; col.g = 0.; col.b = 1.; col.a = 1.;
+
+                            marker_pub.publish(makeMarker(2000, 
+                                                "sandtray", 
+                                                col, 
+                                                gaze_projection[0], gaze_projection[1],0,
+                                                "active_zones"));
+
+                            playground_builder::AttentionTarget target;
+                            target.modality = playground_builder::AttentionTarget::VISUAL;
+                            target.intensity = 1.0;
+                            target.frame_id = kv.first;
+                            targets.targets.push_back(target);
+                        }
+                    }
+                    //////////////////////////////////////////
+
                     map<std::string, std::chrono::system_clock::time_point> seen_frames;
 
                     for(size_t i = 0 ; i < frames.size(); ++i) {
                         auto now = std::chrono::system_clock::now();
-                        auto intensity_distance = fieldOfViewDistance(listener, frames[i], frame);
+                        auto intensity_distance = intensityOfVisualAttention(listener, 
+                                                                             frames[i],
+                                                                             frame);
                         if(intensity_distance > 0.001) {
 
                             if (last_seen_frames.count(frames[i])) {
@@ -229,7 +377,6 @@ int main( int argc, char** argv )
                                                              << ", duration: " << intensity_duration
                                                              << ", final: " << intensity);
 
-                                    std_msgs::ColorRGBA col;
                                     col.r = 1.; col.g = 1.; col.b = 0.; col.a = intensity;
                                     marker_pub.publish(makeMarker(i, frames[i], col));
 
